0a22de869d08b78cdc561316ee7ea5038615fab0
Jef-king <147884623+Jef-king@users.noreply.github.com>
2024-04-02 16:32:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+    var colorSelect = document.getElementById('colorSelect');
+    document.querySelector('input[type="button"]').addEventListener('click', function() {
+        var selectedOption = colorSelect.options[colorSelect.selectedIndex];
+        colorSelect.removeChild(selectedOption);
+    });
+});
